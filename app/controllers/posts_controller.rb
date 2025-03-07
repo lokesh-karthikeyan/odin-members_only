8b4f7c799ed484f6a4a97fbb8f7e982621aa1780
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-03-08 00:00:28 +0530

    Controller: Add `#new, #create` actions for `Posts`

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,20 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [ :new, :create ]
+
+  def new = (@post = Post.new)
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to posts_path, success: "Post was successfully created."
+    else
+      flash[:error] = "An error occured!"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params = params.expect(:post, [ :title, :content ])
 end
