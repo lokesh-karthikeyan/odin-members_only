e7419c21e693ee5397ebe36add67771f5d265db8
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-03-07 21:49:38 +0530

    Model: Add "validations" to persist data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :posts
+
+  validates :name, presence: true, length: { minimum: 4, maximum: 50 }
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
 end
